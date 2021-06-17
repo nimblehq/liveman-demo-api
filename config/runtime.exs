b31aadc7b5f5eb73556e3e3d1459f44c8beb6490
Olivier Robert <olivierobert@gmail.com>
2021-06-17 13:46:57 +0700

    Fix application module name The danger of copy/pasting 🤦‍♂️

--- config/runtime.exs
+++ config/runtime.exs
@@ -5,7 +5,7 @@
 import Config
 
 if config_env() == :prod do
-  config :liveman, Centauri.Repo,
+  config :liveman, Liveman.Repo,
     ssl: true,
     url: System.fetch_env!("DATABASE_URL"),
     pool_size: String.to_integer(System.get_env("DATABASE_POOL_SIZE") || "10")
