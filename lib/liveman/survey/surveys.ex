6f42000a18144b47583541eb9f89284f0f811196
Junan Chakma <junan@nimblehq.co>
2021-07-01 19:11:12 +0700

    More clean up

--- lib/liveman/survey/surveys.ex
+++ lib/liveman/survey/surveys.ex
@@ -1,80 +1,69 @@
 defmodule Liveman.Survey.Surveys do
   use Ecto.Schema
 
-  alias Liveman.Repo
   alias Liveman.Survey.Schemas.{Survey, Question}
 
   @survey_json "priv/repo/data/surveys.json"
 
   def list_surveys do
-    json_survey_list = get_json(@survey_json)
+    survey_list_json = get_json(@survey_json)
 
-    Enum.map(json_survey_list, fn survey ->
-      result = %Survey{
-        id: survey["id"],
-        title: survey["title"],
-        description: survey["description"],
-        thank_email_above_threshold: survey["thank_email_above_threshold"],
-        thank_email_below_threshold: survey["thank_email_below_threshold"],
-        is_active: survey["is_active"],
-        cover_image_url: survey["cover_image_url"],
-        created_at: survey["created_at"],
-        active_at: survey["active_at"],
-        inactive_at: survey["inactive_at"],
-        type: survey["type"]
-      }
+    Enum.map(survey_list_json, fn survey_json ->
+      survey = build_survey(survey_json)
+      questions = build_question(survey_json["questions"], survey)
 
-      questions =
-        Enum.map(survey["questions"], fn question ->
-          %Question{
-            id: question["id"],
-            survey_id: survey["id"],
-            text: question["text"],
-            help_text: question["help_text"],
-            display_order: question["display_order"],
-            short_text: question["short_text"],
-            pick: question["pick"],
-            display_type: question["display_type"],
-            is_mandatory: question["is_mandatory"],
-            correct_answer_id: question["correct_answer_id"],
-            facebook_profile: question["facebook_profile"],
-            twitter_profile: question["twitter_profile"],
-            image_url: question["image_url"],
-            cover_image_url: question["cover_image_url"],
-            cover_image_opacity: question["cover_image_opacity"],
-            cover_background_color: question["cover_background_color"],
-            is_shareable_on_facebook: question["is_shareable_on_facebook"],
-            is_shareable_on_twitter: question["is_shareable_on_twitter"],
-            font_face: question["font_face"],
-            font_size: question["font_size"],
-            tag_list: question["tag_list"]
-          }
-        end)
-
-      # # j = Repo.preload(result, :questions)
-
-      j =
-        result
-        |> Ecto.Changeset.change()
-        |> Ecto.Changeset.put_embed(:questions, questions)
-        |> Ecto.Changeset.apply_changes()
-
-      IO.puts("*****************************")
-      # IO.inspect(questions, label: "question")
-      IO.inspect(j, label: "Result")
-      IO.puts("*****************************")
-
-      # # result = Repo.reload(result)
-
-      # IO.puts("*****************************")
-      # IO.inspect(result, label: "Result")
-      # IO.puts("*****************************")
-
-      j
+      survey
+      |> Ecto.Changeset.change()
+      |> Ecto.Changeset.put_embed(:questions, questions)
+      |> Ecto.Changeset.apply_changes()
     end)
   end
 
-  def get_json(filename) do
+  defp get_json(filename) do
     with {:ok, body} <- File.read(filename), {:ok, json} <- Jason.decode!(body), do: {:ok, json}
+  end
+
+  defp build_survey(json) do
+    %Survey{
+      id: json["id"],
+      title: json["title"],
+      description: json["description"],
+      thank_email_above_threshold: json["thank_email_above_threshold"],
+      thank_email_below_threshold: json["thank_email_below_threshold"],
+      is_active: json["is_active"],
+      cover_image_url: json["cover_image_url"],
+      created_at: json["created_at"],
+      active_at: json["active_at"],
+      inactive_at: json["inactive_at"],
+      type: json["type"]
+    }
+  end
+
+  def build_question(question_json, survey) do
+    Enum.map(question_json, fn question ->
+      %Question{
+        id: question["id"],
+        survey_id: survey.id,
+        text: question["text"],
+        help_text: question["help_text"],
+        display_order: question["display_order"],
+        short_text: question["short_text"],
+        pick: question["pick"],
+        display_type: question["display_type"],
+        is_mandatory: question["is_mandatory"],
+        correct_answer_id: question["correct_answer_id"],
+        facebook_profile: question["facebook_profile"],
+        twitter_profile: question["twitter_profile"],
+        image_url: question["image_url"],
+        cover_image_url: question["cover_image_url"],
+        cover_image_opacity: question["cover_image_opacity"],
+        cover_background_color: question["cover_background_color"],
+        is_shareable_on_facebook: question["is_shareable_on_facebook"],
+        is_shareable_on_twitter: question["is_shareable_on_twitter"],
+        font_face: question["font_face"],
+        font_size: question["font_size"],
+        tag_list: question["tag_list"]
+      }
+    end)
   end
 end
