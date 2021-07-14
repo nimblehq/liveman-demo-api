ed57ecc903f2554ae8c88ad0649ff979f167be14
Junan Chakma <junan@nimblehq.co>
2021-07-14 16:01:54 +0700

    Use meaningful function name

--- lib/liveman/survey/surveys.ex
+++ lib/liveman/survey/surveys.ex
@@ -7,14 +7,14 @@ defmodule Liveman.Survey.Surveys do
   @json_file "priv/repo/data/surveys.json"
 
   def list_surveys do
-    survey_list_json = get_json()
+    survey_list_json = fecth_surveys_from_file()
 
     Enum.map(survey_list_json, fn survey_json ->
       build_survey(survey_json)
     end)
   end
 
-  def get_json do
+  defp fecth_surveys_from_file do
     {_, body} = File.read(@json_file)
     Jason.decode!(body)
   end
