a2e9091084c7ff6119396cf35c59640725139b6d
Junan Chakma <junan@nimblehq.co>
2021-07-16 08:46:33 +0700

    Disable  Consistency.MultiAliasImportRequireUse.MultiAliasImportRequireUse rule

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,6 +2,5 @@ defmodule Liveman.Factory do
   use ExMachina.Ecto, repo: Liveman.Repo
 
   use Liveman.User.UserFactory
-  # credo:disable-for-next-line Credo.Check.Consistency.MultiAliasImportRequireUse
   use Liveman.Survey.{SurveyFactory, QuestionFactory}
 end
