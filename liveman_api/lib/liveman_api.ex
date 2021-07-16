28c5d9d8095fbbf9826dd7e6f2b90aa4537c746c
Terone Madhanayaka <34730459+bterone@users.noreply.github.com>
2021-07-16 17:50:03 +0700

    [4] Create Liveman API request client wrapper (#23)
    * [4] Add API wrapper in LivemanAPI
    
    * Add extra host and docker ip command to dockerhost
    
    * Update the command so it properly exports the variable to the docker-compose file
    
    * Update API wrapper to not require creating a client
    
    * Remove close brace variable
    
    * Update livebook
    
    * Update headings
    
    * Add description

--- liveman_api/lib/liveman_api.ex
+++ liveman_api/lib/liveman_api.ex
@@ -1,18 +1,44 @@
 defmodule LivemanApi do
   @moduledoc """
-  Documentation for `LivemanApi`.
+  API Wrapper for Liveman
   """
 
-  @doc """
-  Hello world.
+  @timeout 30_000
 
-  ## Examples
-
-      iex> LivemanApi.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def get(base_url, url, query \\ %{}) do
+    base_url
+    |> client()
+    |> Tesla.get(url, query: Map.to_list(query))
+    |> handle_response()
   end
+
+  def post(base_url, url, body \\ %{}) do
+    base_url
+    |> client()
+    |> Tesla.post(url, body)
+    |> handle_response()
+  end
+
+  defp client(base_url, opts \\ %{}) do
+    headers = opts[:headers] || []
+
+    middleware = [
+      {Tesla.Middleware.BaseUrl, base_url},
+      Tesla.Middleware.JSON,
+      {Tesla.Middleware.Headers, [{"Content-Type", "application/json"} | headers]},
+      {Tesla.Middleware.Timeout, [timeout: @timeout]}
+    ]
+
+    adapter = {Tesla.Adapter.Hackney, [recv_timeout: @timeout]}
+
+    Tesla.client(middleware, adapter)
+  end
+
+  defp handle_response({:ok, %{status: status, body: body}}) when status in 200..299,
+    do: {:ok, body}
+
+  defp handle_response({:ok, %{status: status, body: body}}),
+    do: {:error, %{status: status, body: body}}
+
+  defp handle_response({:error, reason}), do: {:error, reason}
 end
