180e2e7860dc2f16b53d85284d0d2f97922e52db
Junan Chakma <junan@nimblehq.co>
2021-07-20 12:06:30 +0700

    Remove length checking

--- lib/liveman_web/controllers/plug/auth.ex
+++ lib/liveman_web/controllers/plug/auth.ex
@@ -12,8 +12,8 @@ defmodule LivemanWeb.Plug.Auth do
       |> Enum.at(0)
 
     # If no access_token header key, then access_token value is nil
-    # If access_token header key is present, but value is empty then access_token length is zero
-    if access_token == nil || String.length(String.trim(access_token)) == 0 do
+    # If access_token header key is present, but value is empty then access_token value is empty string
+    if access_token == nil || String.trim(access_token) == "" do
       conn
       |> put_status(:unauthorized)
       |> render(ErrorView, "error.json", errors: [%{detail: "Authentication failed"}])
