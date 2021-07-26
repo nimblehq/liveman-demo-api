4bd83fad3e5ffe497ade3e2506a78b36635a9691
Junan Chakma <junan@nimblehq.co>
2021-07-26 11:35:48 +0700

    Add user request test

--- test/liveman_web/requests/v1/user_request_test.exs
+++ test/liveman_web/requests/v1/user_request_test.exs
@@ -21,4 +21,67 @@ defmodule LivemanWeb.V1UserRequestTest do
              }
     end
   end
+
+  describe "POST /v1/users/signup" do
+    test "returns a success response when the request params are valid", %{conn: conn} do
+      params = %{"email" => "john@doe.com", "password" => "secret"}
+
+      conn =
+        conn
+        |> authenticated_api_conn
+        |> post(Routes.api_v1_user_path(conn, :create, params))
+
+      assert json_response(conn, 200) == %{
+               "meta" => %{"detail" => "An confirmation email has been sent with an OTP code"}
+             }
+    end
+
+    test "returns an error response when the request params are invalid", %{conn: conn} do
+      params = %{"email" => "", "password" => ""}
+
+      conn =
+        conn
+        |> authenticated_api_conn
+        |> post(Routes.api_v1_user_path(conn, :create, params))
+
+      assert json_response(conn, 422) == %{
+               "errors" => [%{"detail" => "Email and Password cannot be blank"}]
+             }
+    end
+  end
+
+  describe "POST /v1/users/verify" do
+    test "returns the user data when the request params are valid", %{conn: conn} do
+      params = %{"otp" => "secret"}
+
+      conn =
+        conn
+        |> authenticated_api_conn
+        |> post(Routes.api_v1_user_path(conn, :verify, params))
+
+      assert %{
+               "data" => %{
+                 "attributes" => %{
+                   "avatar_url" => _,
+                   "email" => _
+                 },
+                 "id" => _,
+                 "relationships" => %{},
+                 "type" => "user"
+               },
+               "included" => []
+             } = json_response(conn, 200)
+    end
+
+    test "returns an error response when the request params are invalid", %{conn: conn} do
+      params = %{"otp" => ""}
+
+      conn =
+        conn
+        |> authenticated_api_conn
+        |> post(Routes.api_v1_user_path(conn, :verify, params))
+
+      assert json_response(conn, 422) == %{"errors" => [%{"detail" => "OTP cannot be blank"}]}
+    end
+  end
 end
